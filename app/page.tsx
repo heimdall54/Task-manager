c1439e6e55f6d0e17439b460ac7dcfed2324eb13
Thomas keim <keim.thomas54@gmail.com>
2024-05-27 14:58:19 +0200

    the first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import styles from "@/styles/page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Hello world 1</h1>
+    </main>
+  );
+}
